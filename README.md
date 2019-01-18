05dbcc77b3d8d26815fe7b92c3cdeff4e819ac3e
Outvi V <git@outv.im>
2019-01-18 20:14:00 +0800

    Fix directory structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# sum from 1 to x
+
 Whatever.
 
-![](https://img.shields.io/travis-ci/pn1/sum)
+![Build Status](https://img.shields.io/travis/outloudvi/sum_from_1_to_x.svg
---
+++ tests/test_sum.py
@@ -0,0 +1,19 @@
+import unittest
+from hello import sum_from_one_to_x
+
+
+class sum_test(unittest.TestCase):  # Testcases
+    def test_numbers(self):  # Basic tests
+        self.assertEqual(sum_from_one_to_x(3), 6)
+        self.assertEqual(sum_from_one_to_x(1), 1)
+        self.assertEqual(sum_from_one_to_x(9), 45)
+
+    def test_error(self):  # Edge-case tests
+        with self.assertRaises(NotImplementedError):
+            sum_from_one_to_x(-1)
+        with self.assertRaises(TypeError):
+            sum_from_one_to_x(1.3)
+
+
+if __name__ == "__main__":
+    unittest.main()
